a169f0912716b8c1a659756436a8bc9519187cd3
Théo RICHARD <theo.richard@omniadefi.com>
2022-11-09 17:24:31 +0100

    feat(ui): table to display posts

---
+++ src/components/Table.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
+	import LinearProgress from '@smui/linear-progress';
+	import Button from '@smui/button';
+
+	export let items: any[] = [];
+	export let loaded: boolean = false;
+</script>
+
+<DataTable table$aria-label="User list" style="width: 100%;">
+	<Head>
+		<Row>
+			<Cell numeric>ID</Cell>
+			<Cell>Title</Cell>
+			<Cell>Image</Cell>
+			<Cell>Date Created</Cell>
+			<Cell>Actions</Cell>
+		</Row>
+	</Head>
+	<Body>
+		{#each items as item (item.id)}
+			<Row>
+				<Cell numeric>{item.id}</Cell>
+				<Cell>{item.title}</Cell>
+				<Cell><img width="100" src={item.image?.[0]?.url} alt="" /></Cell>
+				<Cell>{item.createdAt}</Cell>
+				<Cell>
+					<a href={`/post/${item.id}`}>Edit</a>
+					<Button>Delete</Button>
+				</Cell>
+			</Row>
+		{/each}
+	</Body>
+
+	<LinearProgress
+		indeterminate
+		bind:closed={loaded}
+		aria-label="Data is being loaded..."
+		slot="progress"
+	/>
+</DataTable>
