6fe5a4712450680baa71da69a924a77e058118dc
Théo RICHARD <theo.richard@omniadefi.com>
2022-11-09 17:31:44 +0100

    feat: add `add new` button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,46 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import Table from "../components/Table.svelte"
+	import { onMount } from 'svelte';
+	import Button from '@smui/button';
+	import Table from '../components/Table.svelte';
+	import Dialog from '../components/Dialog.svelte';
 
-  type Post = {
-    createdAt: Date;
-    image: any;
-    content: string;
-    title: string;
-    id: number;
-   };
+	type Post = {
+		createdAt: Date;
+		image: any;
+		content: string;
+		title: string;
+		id: number;
+	};
 
-    let items: Post[] = [];
-    let loaded = false;
+	let items: Post[] = [];
+	let loaded = false;
+	let open = false;
 
-    onMount(() => loadThings(false))
+	onMount(() => loadThings(false));
 
-    function loadThings(wait: boolean) {
-            if (typeof fetch !== 'undefined') {
-                loaded = false;
+	function loadThings(wait: boolean) {
+		if (typeof fetch !== 'undefined') {
+			loaded = false;
 
-                fetch('https://api.fake-rest.refine.dev/posts')
-                    .then((response) => response.json())
-                    .then((json) =>
-                        setTimeout(
-                            () => {
-                                items = json;
-                                loaded = true;
-                            },
-                            // Simulate a long load time.
-                            wait ? 2000 : 0
-                        )
-                    );
-            }
-    }
+			fetch('https://api.fake-rest.refine.dev/posts')
+				.then((response) => response.json())
+				.then((json) =>
+					setTimeout(
+						() => {
+							items = json;
+							loaded = true;
+						},
+						// Simulate a long load time.
+						wait ? 2000 : 0
+					)
+				);
+		}
+	}
 </script>
 
-<Table items={items} loaded={loaded}/>
+<div style="display:flex; justify-content:space-between">
+	<Button on:click={() => (open = true)}>Add New</Button>
+</div>
+<Table {items} {loaded} />
+
+<Dialog {open} />
